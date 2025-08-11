3ac2ba4d7b8b80d1e0f346555d0a953b3bfb78ad
Nercus <48932939+Nercus@users.noreply.github.com>
2025-08-11 14:27:48 +0200

    feat: make list rendering faster with virtual list and removal of dimensions

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="flex flex-col gap-2 p-2 w-full h-full overflow-y-auto">
-    <FileListEntry v-for="path in paths" :key="path" :path="path" />
+  <div v-bind="containerProps" class="flex flex-col gap-2 p-2 w-full h-full">
+    <div v-bind="wrapperProps">
+      <FileListEntry v-for="path in list" :key="path.index" :path="path.data" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useVirtualList } from '@vueuse/core'
 import { useFilesStore } from '../store/store'
 
 const fileStore = useFilesStore()
 const { paths } = storeToRefs(fileStore)
+
+const { list, containerProps, wrapperProps } = useVirtualList(
+  paths,
+  {
+    itemHeight: 56,
+  },
+)
 </script>
--- src/components/FileListEntry.vue
+++ src/components/FileListEntry.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="flex flex-row justify-between items-center bg-base-200/70 hover:bg-base-200/100 px-4 py-2 rounded transition-colors" :class="{ 'opacity-50': isConverting || isLoading }">
-    <div class="flex flex-col justify-start">
-      <span class="font-semibold text-md">{{ fileName }} ({{ imgDimensions?.height }} x {{ imgDimensions?.width }})</span>
-      <span class="text-xs">{{ props.path }}</span>
+  <div class="flex flex-row justify-between items-center bg-base-200/70 hover:bg-base-200/100 mb-2 px-4 py-2 rounded h-14 transition-colors" :class="{ 'opacity-50': isConverting }">
+    <div class="flex flex-col justify-start w-full overflow-hidden">
+      <span class="w-full overflow-ellipsis font-semibold text-md line-clamp-1">{{ fileName }}</span>
+      <span class="overflow-ellipsis text-xs line-clamp-1">{{ props.path }}</span>
     </div>
-    <SolarBlackHoleLinear v-if="(isConverting || isLoading)" class="animate-spin" />
-    <button v-if="!isCompleted && !isErrored" class="btn btn-soft btn-square btn-sm btn-ghost" :disabled="isConverting || isLoading" @click="filesStore.removePath(props.path)">
+    <SolarShieldCrossLinear v-if="isCompleted === false" class="size-6 text-error" />
+    <SolarVerifiedCheckLinear v-else-if="isCompleted === true" class="size-6 text-success" />
+    <button v-else class="btn btn-soft btn-square btn-sm btn-ghost" :disabled="isConverting" @click="filesStore.removePath(props.path)">
       <SolarListCrossMinimalisticLinear />
     </button>
-    <SolarVerifiedCheckLinear v-else-if="isCompleted" class="size-6 text-success" />
-    <SolarShieldCrossLinear v-else-if="isErrored" class="size-6 text-error" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { readFile } from '@tauri-apps/plugin-fs'
 import { useFilesStore } from '../store/store'
 
 const props = defineProps<{
@@ -22,40 +20,13 @@ const props = defineProps<{
 }>()
 
 const filesStore = useFilesStore()
-const { isConverting, completed, errored } = storeToRefs(filesStore)
+const { isConverting, completed } = storeToRefs(filesStore)
 
 const isCompleted = computed(() => {
-  return completed.value.has(props.path)
-})
-
-const isErrored = computed(() => {
-  return errored.value.has(props.path)
+  return completed.value.get(props.path)
 })
 
 const fileName = computed(() => {
   return props.path.split('/').pop() || props.path
 })
-
-const isLoading = shallowRef(false)
-
-const imgDimensions = computedAsync(async () => {
-  const imageData = await readFile(props.path)
-  // Create a Blob from the binary data
-  const blob = new Blob([imageData as BlobPart], { type: 'image/*' })
-  // Create an object URL for the Blob
-  const url = URL.createObjectURL(blob)
-  // Load the image and get its dimensions
-  return new Promise<{ width: number, height: number }>((resolve, reject) => {
-    const img = new Image()
-    img.onload = () => {
-      resolve({ width: img.width, height: img.height })
-      URL.revokeObjectURL(url)
-    }
-    img.onerror = (err) => {
-      reject(err)
-      URL.revokeObjectURL(url)
-    }
-    img.src = url
-  })
-}, null, isLoading)
 </script>
