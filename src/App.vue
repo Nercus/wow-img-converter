4ee1c6181f17c9192df8cb46951d9725d9e548bc
Nercus <48932939+Nercus@users.noreply.github.com>
2025-08-11 14:26:37 +0200

    chore: minor styling changes

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,29 @@
   </Notivue>
   <div class="flex flex-col bg-base-100 size-full">
     <div class="flex flex-row justify-between items-center bg-base-100 border-b border-base-content/20 navbar">
-      <h1 class="text-xl">
-        Nerc's BLP Converter
-      </h1>
+      <span>
+        <h1 class="text-xl">
+          WoW Image Converter
+        </h1>
+        <h2 class="pl-4 text-xs italic">
+          by <a href="https://github.com/Nercus" target="_blank">Nerc</a>
+        </h2>
+      </span>
       <button class="btn btn-soft btn-ghost btn-sm" @click="filesStore.reset()">
         <SolarRestartLinear class="-scale-x-100" />
-        Reset
       </button>
     </div>
     <div class="relative flex flex-col flex-1 justify-center items-center overflow-hidden">
       <DropZone class="absolute inset-0" @drop="onFilesDropped" />
       <span v-if="paths.length === 0" class="flex items-center h-full">Drop files here to convert</span>
-      <FileList class="flex-1" />
+      <FileList />
     </div>
     <Toolbar />
   </div>
 </template>
 
 <script setup lang="ts">
+import { Notification, Notivue } from 'notivue'
 import { useFilesStore } from './store/store'
 
 const filesStore = useFilesStore()
